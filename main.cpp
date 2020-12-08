7e5f134656ab920e5adc34cbb86098ab4767a4aa
ForlornScout <71358252+ForlornScout@users.noreply.github.com>
2020-12-07 19:58:36 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char operation;
+    float num1, num2;
+
+    cout << "Enter sign: ";
+    cin >> operation;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(operation)
+    {
+        case '+':
+            cout << num1 + num2;
+            break;
+
+        case '-':
+            cout << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 * num2;
+            break;
+
+        case '/':
+            cout << num1 / num2;
+            break;
+    }
+
+    return 0;
+}
